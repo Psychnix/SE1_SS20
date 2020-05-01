cb746b8ee31b9d0bc58ea3303624fca7a1393fe3
psychnix <tobiaspsi96@yahoo.de>
2020-05-01 18:28:29 +0200

    Employer angefangen

--- SE1_Semesterprojekt/src/Employee.java
+++ SE1_Semesterprojekt/src/Employee.java
@@ -1,4 +1,11 @@
-
+//import java.util.*;
+import java.lang.Object;
+import java.time.*;
 public class Employee {
-
+	private LocalDateTime ShiftStart;
+	
+	public Employee(LocalDateTime a) {
+		ShiftStart = a;
+	}
+	x = LocalDateTime.of(2019, 04, 12, 13, 23, 7);
 }
