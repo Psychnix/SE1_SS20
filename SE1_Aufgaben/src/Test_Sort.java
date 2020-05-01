5533081dd9f55174988b55e20e51e4b4ab1128a0
Joshua <joshua.hahn@smail.inf.h-brs.de>
2020-05-01 16:33:14 +0200

    test cases for 2.2.2

--- SE1_Aufgaben/src/Test_Sort.java
+++ SE1_Aufgaben/src/Test_Sort.java
@@ -6,7 +6,22 @@ public class Test_Sort {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String s[] = { "A" , "B", "C", "D" };
+		boolean result = Sort.isWellSorted(s);
+		assertEquals(true, result);
+		String s1[] = { "B" , "A", "C", "D" };
+		result = Sort.isWellSorted(s1);
+		assertEquals(true, result);
+		String s2[]= { "A" , "A", "C", "D" };
+		result = Sort.isWellSorted(s2);
+		assertEquals(true, result);
+		String s3[] = { "C" , "B", "A", "D" };
+		result = Sort.isWellSorted(s3);
+		assertEquals(false, result);
+		String s4[]= { "A" , "B", "D", "C" };
+		result = Sort.isWellSorted(s4);
+		assertEquals(false, result);
+
 	}
 
 }
