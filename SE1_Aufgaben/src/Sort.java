bbe692ad17fcbc14b782d5f9edd7c0f4f12be101
psychnix <tobiaspsi96@yahoo.de>
2020-05-01 15:45:43 +0200

    Methode isWellSorted (String[] sequence) erstellt und fertig

--- SE1_Aufgaben/src/Sort.java
+++ SE1_Aufgaben/src/Sort.java
@@ -1,4 +1,22 @@
 
 public class Sort {
-
+	//Variablen
+	private static String[][] pattern = new String [][] {{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+	
+	public static boolean isWellSorted (String[] sequence){
+		int a = pattern[0].length;
+		
+		for(int i1=0; i1<sequence.length; i1++) {
+			String sequence_exemplar1 = sequence[i1];
+			for(int i2=i1+1;i2<sequence.length;i2++) {
+				String sequence_exemplar2 = sequence[i2];
+				for(int j=0; j<a; j++) {
+					if(sequence_exemplar1.contentEquals(pattern[j][1]) && sequence_exemplar2.contentEquals(pattern[j][0])) {
+						return false;
+					}
+				}
+			}
+		}	
+		return true;
+	}
 }
