61efb3a970967ffa663e0c5b4f7ef162bb5dcebe
Joshua <joshua.hahn@smail.inf.h-brs.de>
2020-05-01 18:30:25 +0200

    Erstellen der Methoden des ersten Scrum sprints

--- SE1_Semesterprojekt/src/ParkingSpot.java
+++ SE1_Semesterprojekt/src/ParkingSpot.java
@@ -1,4 +1,29 @@
 
 public class ParkingSpot {
-
+	private static int numberOfParkingSpots = 10;
+	private static int fullParkingSpots = 0;
+	
+	public ParkingSpot(int allParkingSpots) {
+		numberOfParkingSpots = allParkingSpots;
+	}
+	
+	//Returns the avaiable number of parking spots
+	public static int avaiableParkingSpots ()
+	{
+		return numberOfParkingSpots-fullParkingSpots;
+	}
+	
+	//Raise the number of full parking spots
+	public static void raiseFullSpots (int i)
+	{
+		fullParkingSpots++;
+	}
+	
+	//Lower the number of full parking spots
+		public static void lowerFullSpots (int i)
+		{
+			fullParkingSpots--;
+		}
+	
+	
 }
--- SE1_Semesterprojekt/src/ParkingTicket.java
+++ SE1_Semesterprojekt/src/ParkingTicket.java
@@ -1,4 +1,32 @@
+import java.time.*;
 
 public class ParkingTicket {
+	private static int ticketNumber = 0;
+	private static int ticketCategorie = 0;
+    private static LocalDateTime ticketTime;
 
+	
+	public ParkingTicket(int iticketNumber, int iticketCategorie ) {
+		ticketNumber = iticketNumber;
+		ticketCategorie = iticketCategorie;
+		ticketTime = LocalDateTime.now();
+	}
+	
+	//Returns the ticket number
+	public static int returnTicketNumber()
+	{
+		return ticketNumber;
+	}
+	
+	//Returns the ticket categorie
+	public static int returnTicketCategorie()
+	{
+		return ticketCategorie;
+	}
+	
+	//Returns the ticket categorie
+	public static LocalDateTime returnTicketTime()
+	{
+		return ticketTime;
+	}
 }
