55f232eb39f8b128478c9442dc645b7203a3c386
Joshua <joshua.hahn@smail.inf.h-brs.de>
2020-05-03 12:29:51 +0200

    Anpassen des tests um das Datum des Parktickets zu prüfen

--- SE1_Semesterprojekt/src/FirstTests.java
+++ SE1_Semesterprojekt/src/FirstTests.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.Test;
 
 class FirstTests {
@@ -7,11 +10,17 @@ class FirstTests {
 	//test for the calss Parking Ticket
 		@Test 
 		void test_ParkingTicket() {
+			LocalDateTime ticketTime = LocalDateTime.now();
 			ParkingTicket ticket = new ParkingTicket(1,0);
 			int result = ticket.returnTicketCategorie();
 			assertEquals(0, result);
 			result = ticket.returnTicketNumber();
 			assertEquals(1, result);
+			LocalDateTime timeresult = ticket.returnTicketTime();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			String dateTimeString = ticketTime.format(formatter);  //2019-03-28 14:47:33
+			String dateTimeString2 = timeresult.format(formatter);  //2019-03-28 14:47:33
+			assertEquals(dateTimeString2, dateTimeString);
 		}
 		//test for the class ParkingSpot
 		@Test 
