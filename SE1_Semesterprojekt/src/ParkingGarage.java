6f254660e3b7d3a90dc50799b288d0b02ba0380a
psychnix <tobiaspsi96@yahoo.de>
2020-05-03 13:47:31 +0200

    In Employee Variablen und Formatierung angepasst: neue Variable "name" In ParkingSpot return für die setteer eingefügt In Parking Garage mit Programmierung begonnen.

--- SE1_Semesterprojekt/src/ParkingGarage.java
+++ SE1_Semesterprojekt/src/ParkingGarage.java
@@ -1,4 +1,22 @@
+import java.time.*;
+import java.util.Random;
 
 public class ParkingGarage {
+/* ## BEGIN Variablen Deklaration ## */
+	public Employee[] employers;
+/* ## END Variablen Deklaration ## */
 	
+
+	
+/* BEGIN ## Funktionen ## */
+	public static int getRandNumInRange(int min, int max) {
+        Random r = new Random();
+        return r.ints(min, (max + 1)).limit(1).findFirst().getAsInt();
+    }
+	
+	public LocalDateTime hireEmployee() {
+		return null;
+	}
+/* END ## Funktionen ## */
+
 }
--- SE1_Semesterprojekt/src/ParkingSpot.java
+++ SE1_Semesterprojekt/src/ParkingSpot.java
@@ -10,27 +10,27 @@ public class ParkingSpot {
 	}
 	
 	//Returns the avaiable number of parking spots
-	public  int avaiableParkingSpots ()
+	public int avaiableParkingSpots ()
 	{
 		return numberOfParkingSpots-fullParkingSpots;
 	}
 	
 	//Returns the categorie of the parking spots
-	public  int categorieParkingSpots ()
+	public int categorieParkingSpots ()
 	{
 		return parkingSpotCategorie;
 	}
 	
 	//Raise the number of full parking spots
-	public  void raiseFullSpots ()
+	public int raiseFullSpots ()
 	{
-		fullParkingSpots++;
+		return fullParkingSpots++;
 	}
 	
 	//Lower the number of full parking spots
-	public  void lowerFullSpots ()
+	public int lowerFullSpots ()
 	{
-		fullParkingSpots--;
+		return fullParkingSpots--;
 	}
 	
 	
