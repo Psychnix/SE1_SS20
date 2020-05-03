664e61978f5d6bbd71e978de1561e3ae9b10b531
Joshua <joshua.hahn@smail.inf.h-brs.de>
2020-05-03 12:00:53 +0200

    tests erstellt für ParkingTicket und ParkingSpot

--- SE1_Semesterprojekt/src/FirstTests.java
+++ SE1_Semesterprojekt/src/FirstTests.java
@@ -4,9 +4,30 @@ import org.junit.jupiter.api.Test;
 
 class FirstTests {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	//test for the calss Parking Ticket
+		@Test 
+		void test_ParkingTicket() {
+			ParkingTicket ticket = new ParkingTicket(1,0);
+			int result = ticket.returnTicketCategorie();
+			assertEquals(0, result);
+			result = ticket.returnTicketNumber();
+			assertEquals(1, result);
+		}
+		//test for the class ParkingSpot
+		@Test 
+		void test_ParkingSpot() {
+			ParkingSpot spot = new ParkingSpot(25,1);
+			int result = spot.categorieParkingSpots();
+			assertEquals(1, result);
+			result = spot.avaiableParkingSpots();
+			assertEquals(25, result);
+			spot.raiseFullSpots();
+			spot.raiseFullSpots();
+			result = spot.avaiableParkingSpots();
+			assertEquals(23, result);
+			spot.lowerFullSpots();
+			result = spot.avaiableParkingSpots();
+			assertEquals(24, result);
+		}
 
 }
--- SE1_Semesterprojekt/src/ParkingSpot.java
+++ SE1_Semesterprojekt/src/ParkingSpot.java
@@ -1,29 +1,37 @@
 
 public class ParkingSpot {
-	private static int numberOfParkingSpots = 10;
-	private static int fullParkingSpots = 0;
+	private  int numberOfParkingSpots = 0;
+	private  int parkingSpotCategorie = 0;
+	private  int fullParkingSpots = 0;
 	
-	public ParkingSpot(int allParkingSpots) {
+	public ParkingSpot(int allParkingSpots,int iparkingSpotCategorie) {
 		numberOfParkingSpots = allParkingSpots;
+		parkingSpotCategorie = iparkingSpotCategorie;
 	}
 	
 	//Returns the avaiable number of parking spots
-	public static int avaiableParkingSpots ()
+	public  int avaiableParkingSpots ()
 	{
 		return numberOfParkingSpots-fullParkingSpots;
 	}
 	
+	//Returns the categorie of the parking spots
+	public  int categorieParkingSpots ()
+	{
+		return parkingSpotCategorie;
+	}
+	
 	//Raise the number of full parking spots
-	public static void raiseFullSpots (int i)
+	public  void raiseFullSpots ()
 	{
 		fullParkingSpots++;
 	}
 	
 	//Lower the number of full parking spots
-		public static void lowerFullSpots (int i)
-		{
-			fullParkingSpots--;
-		}
+	public  void lowerFullSpots ()
+	{
+		fullParkingSpots--;
+	}
 	
 	
 }
