ed71eb264449656facd3c071feefe6bebac9ea21
psychnix <tobiaspsi96@yahoo.de>
2020-05-12 17:01:05 +0200

    Parkingticket Methoden um request-Daten in einem Ticket-Array abzuspeichern und dieses ggf. auch zu vergrößern.

--- SE1_Semesterprojekt_Servlet/src/ParkingTicket.java
+++ SE1_Semesterprojekt_Servlet/src/ParkingTicket.java
@@ -4,12 +4,40 @@ public class ParkingTicket {
 	private static int ticketNumber = 0;
 	private static int ticketCategorie = 0;
     private static LocalDateTime ticketTime;
-
+    private static String[][] tickets = new String[1][6];
 	
-	public ParkingTicket(int iticketNumber, int iticketCategorie ) {
-		ticketNumber = iticketNumber;
-		ticketCategorie = iticketCategorie;
-		ticketTime = LocalDateTime.now();
+	public static void newEnter(String[] splitBody) {
+		if(Integer.valueOf(splitBody[1]) > tickets.length) {
+			expandArray();
+		}
+		//Enter-Nummer
+		tickets[Integer.valueOf(splitBody[1])][0] = splitBody[1];
+		//Ticket-ID
+		tickets[Integer.valueOf(splitBody[1])][1] = splitBody[5];
+		//Datum & Uhrzeit
+		tickets[Integer.valueOf(splitBody[1])][2] = splitBody[2];
+	}
+	
+	public static void newLeave(String[] splitBody) {
+		//Aufenthaltsdauer
+		tickets[Integer.valueOf(splitBody[1])][3] = splitBody[3];
+		//Preis
+		tickets[Integer.valueOf(splitBody[1])][4] = splitBody[4];
+		//Fahrzeugfarbe
+		tickets[Integer.valueOf(splitBody[1])][5] = splitBody[6];
+	}
+	
+	private static String[][] expandArray(){
+		String[][]temp = new String[tickets.length*2][6];
+		
+		for(int i=0; i<tickets.length; i++) {
+			for(int j=0; j<tickets[0].length; j++) {
+				temp[i][j] = tickets[i][j];
+			}
+		}
+		tickets = temp;
+		
+		return tickets;
 	}
 	
 	//Returns the ticket number
